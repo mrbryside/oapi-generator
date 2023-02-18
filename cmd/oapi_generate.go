d516904a30ae5615e47b292a91962b19bfda0e92
sirawatB <sirawat.is@nocnoc.com>
2023-02-18 15:50:11 +0700

    Update v1.0.8 tag

--- cmd/oapi_generate.go
+++ cmd/oapi_generate.go
@@ -32,17 +32,20 @@ var generateCmd = &cobra.Command{
 		}
 
 		// Parse the YAML data into a map[string]interface{} object
-		var config map[string]interface{}
+		var config map[string]*string
 		err = yaml.Unmarshal(data, &config)
 		if err != nil {
 			log.Fatalf("failed to parse YAML data: %v", err)
 		}
+		if config["spec-dir"] == nil || config["gen-dir"] == nil {
+			return fmt.Errorf("error oapi-cfg.yaml is wrong")
+		}
 
-		yamlFile = fmt.Sprintf("%s/server.cfg.yaml", config["spec-dir"])
+		yamlFile = fmt.Sprintf("%s/server.cfg.yaml", *config["spec-dir"])
 		if _, err := os.Stat(yamlFile); os.IsNotExist(err) {
 			return fmt.Errorf("spec server config (%s) does not exist", yamlFile)
 		}
-		yamlFile = fmt.Sprintf("%s/%s.yaml", config["spec-dir"], name)
+		yamlFile = fmt.Sprintf("%s/%s.yaml", *config["spec-dir"], name)
 		if _, err := os.Stat(yamlFile); os.IsNotExist(err) {
 			return fmt.Errorf("spec file (%s) does not exist", yamlFile)
 		}
@@ -53,11 +56,9 @@ var generateCmd = &cobra.Command{
 			return fmt.Errorf("error running init folder: %v", err)
 		}
 
-		genCmd := exec.Command("make", "generate-server", fmt.Sprintf("name=%s", name), fmt.Sprintf("genPath=%s", config["gen-dir"]), fmt.Sprintf("specPath=%s", config["spec-dir"]))
-		genCmd.Dir = "./"
-		_, err = genCmd.CombinedOutput()
+		err = generateServer(name, *config["gen-dir"], *config["spec-dir"])
 		if err != nil {
-			return fmt.Errorf("error running generate: %v", err)
+			fmt.Printf("error generating server files: %v", err)
 		}
 		fmt.Println("generated")
 		return nil
@@ -66,4 +67,58 @@ var generateCmd = &cobra.Command{
 func init() {
 	generateCmd.Flags().String("name", "", "Name of the application")
 	rootCmd.AddCommand(generateCmd)
+}
+
+func generateServer(name, genPath, specPath string) error {
+	// Remove existing DTO and service folders, and create new ones
+	err := os.RemoveAll(fmt.Sprintf("%s/oapi/%sdto", genPath, name))
+	if err != nil {
+		return fmt.Errorf("error removing DTO folder: %v", err)
+	}
+	err = os.RemoveAll(fmt.Sprintf("%s/oapi/%ssrv", genPath, name))
+	if err != nil {
+		return fmt.Errorf("error removing service folder: %v", err)
+	}
+	err = os.MkdirAll(fmt.Sprintf("%s/oapi/%sdto", genPath, name), os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error creating DTO folder: %v", err)
+	}
+	err = os.MkdirAll(fmt.Sprintf("%s/oapi/%ssrv", genPath, name), os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error creating service folder: %v", err)
+	}
+
+	// Create a temporary server config file and write to it
+	serverCfgPath := fmt.Sprintf("%s/server-%s.cfg.yaml", specPath, name)
+	serverCfgContents := fmt.Sprintf("#name%sdto\n", name)
+	err = ioutil.WriteFile(serverCfgPath, []byte(serverCfgContents), 0644)
+	if err != nil {
+		return fmt.Errorf("error creating temporary server config file: %v", err)
+	}
+
+	// Generate the DTO files using oapi-gen
+	dtoCmd := exec.Command("./oapi-gen", "-generate", "types", "-o", fmt.Sprintf("%s/oapi/%sdto/%sdto.go", genPath, name, name), "-package", fmt.Sprintf("%sdto", name), fmt.Sprintf("%s/%s.yaml", specPath, name))
+	dtoCmd.Dir = "./"
+	_, err = dtoCmd.CombinedOutput()
+	if err != nil {
+		os.Remove(serverCfgPath)
+		return fmt.Errorf("error generating DTO files: %v", err)
+	}
+
+	// Generate the service files using oapi-gen and the temporary config file
+	srvCmd := exec.Command("./oapi-gen", "--config", serverCfgPath, "-package", fmt.Sprintf("%ssrv", name), "-o", fmt.Sprintf("%s/oapi/%ssrv/%ssrv.go", genPath, name, name), fmt.Sprintf("%s/%s.yaml", specPath, name))
+	srvCmd.Dir = "./"
+	_, err = srvCmd.CombinedOutput()
+	if err != nil {
+		os.Remove(serverCfgPath)
+		return fmt.Errorf("error generating service files: %v", err)
+	}
+
+	// Clean up the temporary server config file
+	err = os.Remove(serverCfgPath)
+	if err != nil {
+		return fmt.Errorf("error removing temporary server config file: %v", err)
+	}
+
+	return nil
 }
